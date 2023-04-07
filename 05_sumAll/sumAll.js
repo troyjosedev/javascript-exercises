15b2f3522ecec58aee2e0ffb7d57e6450f6313d1
Troy <HiponaTroyJoseB.dev@gmail.com>
2023-04-08 00:58:04 +0800

    Submit solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
-};
+const sumAll = function (min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR: Both inputs must be integers";
+    if (min < 0 || max < 0) return "ERROR: Both inputs must be non-negative";
+    if (min > max) [min, max] = [max, min];
+    
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+    return sum;
+  };
+  
 
 // Do not edit below this line
 module.exports = sumAll;
